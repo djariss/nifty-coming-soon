86bb1d36e6f255388406c970191555dbe4a74872
Davor Veselinovic <djariss@gmail.com>
2019-07-07 00:08:46 +0200

    Update nifty-coming-soon.php

--- nifty-coming-soon.php
+++ nifty-coming-soon.php
@@ -3,7 +3,7 @@
 Plugin Name: Nifty Coming Soon and Maintenance page
 Plugin URI:  https://wordpress.org/plugins/nifty-coming-soon-and-under-construction-page/
 Description: Easy to setup Coming soon, Maintenance and Under Construction page. It features Responsive design, Countdown timer, Animations, Live Preview, Background Slider, Subscription form and more.
-Version:     1.1.3
+Version:     1.1.4
 Author:      Davor Veselinovic
 Author URI:  https://themeadviser.com/
 License:     GPL2
@@ -25,13 +25,13 @@ function nifty_cs_custom_menu()
     global $wp_admin_bar;
 	$value=ot_get_option( 'coming_soon_mode_on___off' );
     if ( $value != "off" ) {
-		
+
     $argsParent=array(
         'id' => 'niftycs_custom_menu',
         'title' => 'Nifty Coming Soon Enabled',
         'href' => admin_url('?page=niftycs-options#section_general_settings'),
 		'meta'   => array( 'class' => 'red-hot-button' ),
-		
+
     );
     $wp_admin_bar->add_menu($argsParent);
 	}
@@ -76,58 +76,24 @@ add_action('admin_head', 'nifty_cs_admin_custom_colors');
 function nifty_cs_add_dashboard_widgets() {
 
 	wp_add_dashboard_widget(
-                 'nifty_cs_dashboard_widget',        
-                 'Nifty Coming Soon - Need a help?',         
-                 'nifty_cs_dashboard_widget_function' 
-        );	
+                 'nifty_cs_dashboard_widget',
+                 'Nifty Coming Soon - Need a help?',
+                 'nifty_cs_dashboard_widget_function'
+        );
 }
 add_action( 'wp_dashboard_setup', 'nifty_cs_add_dashboard_widgets' );
-
-function nifty_cs_dashboard_widget_function() {
-
-// Message to our beloved users :)
-
-	$nifty_cs_banner_url = 'https://themeadviser.com/static/nifty-coming-soon-admin-baner.png';
-	$nifty_cs_support_url = 'https://wordpress.org/support/plugin/nifty-coming-soon-and-under-construction-page';
-	$nifty_cs_theme1_banner_url = 'https://themeadviser.com/static/pinhole_screenshot.jpg';
-	$nifty_cs_theme1_demo_url = 'http://demo.mekshq.com/?theme=pinhole&utm_source=themeadviser&utm_campaign=nifty-coming-soon';
-	$nifty_cs_theme2_banner_url = 'https://themeadviser.com/static/vlog_screenshot.jpg';
-	$nifty_cs_theme2_demo_url = 'http://demo.mekshq.com/?theme=vlog&utm_source=themeadviser&utm_campaign=nifty-coming-soon';
-	$nifty_cs_theme3_banner_url = 'https://themeadviser.com/static/herald_screenshot.jpg';
-	$nifty_cs_theme3_demo_url = 'http://demo.mekshq.com/?theme=herald&utm_source=themeadviser&utm_campaign=nifty-coming-soon';
-	$nifty_cs_theme4_banner_url = 'https://themeadviser.com/static/voice_screenshot.jpg';
-	$nifty_cs_theme4_demo_url = 'http://demo.mekshq.com/?theme=voice&utm_source=themeadviser&utm_campaign=nifty-coming-soon';
-	$nifty_cs_theme5_banner_url = 'https://themeadviser.com/static/gridlove_screenshot.jpg';
-	$nifty_cs_theme5_demo_url = 'http://demo.mekshq.com/?theme=gridlove&utm_source=themeadviser&utm_campaign=nifty-coming-soon';
-	
-
-	echo "<img style='max-width:100%; margin-top:5px;' src='".$nifty_cs_banner_url."'>";
-	echo "<p>You have succefully installed Nifty Coming soon plugin and you can freely work on your website while the coming soon mode is active. </br> If you need help with the plugin, feel free to drop us the message, <a href=".$nifty_cs_support_url." target='_blank'/>right here</a><hr>In the meantime, you are most likely looking for a <u>good theme</u> to start you work. So, we picked some worthy themes for you if you don't mind. :)</p>";
-	echo "<h3 style='padding-top:10px;'><strong>*** AMAZING 5 STAR RATED THEMES ***</strong></h3>";
-	echo "<div style='width:100%;'>";
-	echo "<div style='width:49%;float:left;padding-top:15px;'><a target='_blank' href='".$nifty_cs_theme1_demo_url."'><img style='max-width:95%;' src='".$nifty_cs_theme1_banner_url."'></a></div><div style='width:49%;float:right;'><p><strong><a target='_blank' href='".$nifty_cs_theme1_demo_url."'>PINHOLE</a></strong></br> Top Tier Gallery theme for Photographers. Perfect gallery theme for WordPress with unique image features.</p></div><div style='clear:both;'></div>";
-	echo "<div style='width:49%;float:left;padding-top:15px;'><a target='_blank' href='".$nifty_cs_theme2_demo_url."'><img style='max-width:95%;' src='".$nifty_cs_theme2_banner_url."'></a></div><div style='width:49%;float:right;'><p><strong><a target='_blank' href='".$nifty_cs_theme2_demo_url."'>VLOG</a></strong></br> Vloggers gonna love this one, imagine fully featured magazine theme specialized in videos and vlogging.</p></div><div style='clear:both;'></div>";
-	echo "<div style='width:49%;float:left;padding-top:15px;'><a target='_blank' href='".$nifty_cs_theme3_demo_url."'><img style='max-width:95%;' src='".$nifty_cs_theme3_banner_url."'></a></div><div style='width:49%;float:right;'><p><strong><a target='_blank' href='".$nifty_cs_theme3_demo_url."'>HERALD</a></strong></br> Start your news portal or magazine in a bold manner. Herald offers everything that successful magazines need.</p></div><div style='clear:both;'></div>";
-	echo "<div style='width:49%;float:left;padding-top:15px;'><a target='_blank' href='".$nifty_cs_theme4_demo_url."'><img style='max-width:95%;' src='".$nifty_cs_theme4_banner_url."'></a></div><div style='width:49%;float:right;'><p><strong><a target='_blank' href='".$nifty_cs_theme4_demo_url."'>VOICE</a></strong></br> Best rated and most popular personal blog / magazine WordPress theme with the trust of a couple of thousands users.</p></div><div style='clear:both;'></div>";
-	echo "<div style='width:49%;float:left;padding-top:15px;'><a target='_blank' href='".$nifty_cs_theme5_demo_url."'><img style='max-width:95%;' src='".$nifty_cs_theme5_banner_url."'></a></div><div style='width:49%;float:right;'><p><strong><a target='_blank' href='".$nifty_cs_theme5_demo_url."'>GRIDLOVE</a></strong></br> Modern material design and grid inspired magazine theme. Powerful and easy to use, 5 star rated.</p></div><div style='clear:both;'></div>";
-	
-	
-	echo "</div>";
-}
-
-
 
 // Redirection starts here
 
 function nifty_cs_redirect()
-        { 
-            
+        {
+
 			// Check if the coming soon mode is enabled in the general settings
-			
+
 			$value = ot_get_option( 'coming_soon_mode_on___off' );
-			
+
 			if ( $value != "off" ) {
-            
+
 				if(!is_feed())
 				{
                 	// Guests are redirected to the coming soon page
@@ -143,7 +109,7 @@ function nifty_cs_redirect()
 	            if (is_user_logged_in() )
                 {
                     // Get logined in user role
-		            global $current_user; 
+		            global $current_user;
 		            $LoggedInUserID = $current_user->ID;
 		            $UserData = get_userdata( $LoggedInUserID );
 		            // If user is not having administrator, editor, author or contributor role he will be server the coming soon page too :)
@@ -159,25 +125,25 @@ function nifty_cs_redirect()
 		        }
             }
 		}
-		
-// Live Preview		
-		
+
+// Live Preview
+
 add_action('init','nifty_cs_get_preview');
 
 function nifty_cs_get_preview ()
 {
-	 
+
 	 if (  (isset($_GET['get_preview']) && $_GET['get_preview'] == 'true') )
 		{
-			
+
 			 $template_path = plugin_dir_path(__FILE__).'template/index.php';
 			 include($template_path);
 			 exit();
 		}
 }
-	 
+
 // Prevent wp-login.php to be blocked by the Coming soon page.
-		
+
 add_action('init','nifty_cs_skip_redirect_on_login');
 
 function nifty_cs_skip_redirect_on_login ()
@@ -185,10 +151,10 @@ function nifty_cs_skip_redirect_on_login ()
 	global $currentpage;
 	if ('wp-login.php' == $currentpage)
 		{
-		return;				
-		} else 
+		return;
+		} else
 			{
-			 add_action( 'template_redirect', 'nifty_cs_redirect' );	
+			 add_action( 'template_redirect', 'nifty_cs_redirect' );
 			}
 }
 
